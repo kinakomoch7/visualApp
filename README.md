deab963906c2c02361129ad2f7a8bba46217cc20
kinakomoch7 <book-insect14@outlook.jp>
2023-09-07 13:55:23 +0900

    データの記載

---
+++ README.md
@@ -0,0 +1,8 @@
+#　東京都オープンデータの可視化
+
+## データの取得方法
+
+東京都のオープンデータから令和 4 年度の公金支出情報を取得（https://catalog.data.metro.tokyo.lg.jp/dataset/t000016d0000000044）
+originalDatas に元データを記載
+
+change.py から json ファイルに変換。
---
+++ public/changeData.py
@@ -0,0 +1,31 @@
+import csv
+import json
+
+
+def csv_to_json(csv_file_path, json_file_path):
+    # CSVファイルを開く
+    with open(csv_file_path, 'r', encoding='utf-8-sig') as csv_file:
+        # CSVを読み込む
+        csv_data = csv.reader(csv_file)
+        headers = next(csv_data)  # ヘッダー行をスキップ
+
+        # JSONデータを作成
+        json_data = []
+        for row in csv_data:
+            json_row = {}
+            for i in range(len(headers)):
+                json_row[headers[i]] = row[i]
+            json_data.append(json_row)
+
+    # JSONファイルに書き込む
+    with open(json_file_path, 'w', encoding='utf-8') as json_file:
+        json.dump(json_data, json_file, indent=4, ensure_ascii=False)
+
+
+# CSVファイルのパス
+csv_file_path = '/Users/koidetomoki/Desktop/opDataJson/opData03.csv'
+# JSONファイルのパス
+json_file_path = '/Users/koidetomoki/Desktop/opDataJson/opData03.json'
+
+# CSVをJSONに変換する
+csv_to_json(csv_file_path, json_file_path)
